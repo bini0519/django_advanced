dbc7a120b9958a281e831f50d8efb2f78f517cf3
subin <qlstn9150@gmail.com>
2020-09-12 14:30:46 +0900

    8장실습

--- firstrest/post/views.py
+++ firstrest/post/views.py
@@ -1,53 +1,38 @@
-from .models import Post
-from .serializer import PostSerializer
-from rest_framework import viewsets
+from post.models import Post
+from post.serializer import PostSerializer
 
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
+from rest_framework import mixins
 
-#CBV
-class PostVeiwSet(viewsets.ModelViewSet):
+class PostList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    #등록(generics.GenericAPIView 안에서 선언된 변수임)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    #필요로 하는 메서드
+    #가변인자 사용(인자의 개수에 상관없이 받음)
+    def get(self, request, *args, **kwargs):
+        #list는 ListModelMixin에서 이미 정의된 메서드
+        return self.list(request, *args, **kwargs)
 
-class PostList(APIView):
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True) #여러개의 객체 처리
-        return Response(serializer.data)
+    def post(self, request, *args, **kwargs):
+        #create는 CreateModelMixin에서 이미 정의된 메서드
+        return self.create(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid(): #유효성 검사
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PostDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-class PostDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
