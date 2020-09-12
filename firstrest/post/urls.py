c70b9bc21cf00fae6421d392cdf9d0f86c0650b3
subin <qlstn9150@gmail.com>
2020-09-12 18:59:41 +0900

    10강 실습완료

--- firstrest/post/urls.py
+++ firstrest/post/urls.py
@@ -1,10 +1,20 @@
-from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from . import views
+#from rest_framework.urlpatterns import format_suffix_patterns
+from post import views
+
+from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('post', views.PostVeiwSet)
+router.register('post', views.PostViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
+urlpatterns=[
+    path('', include(router.urls))
 ]
+
+
+'''
+urlpatterns = [
+    path('post/', views.PostList.as_view()),
+    path('post/<int:pk>/', views.PostDetail.as_view()),
+]
+'''
--- firstrest/post/views.py
+++ firstrest/post/views.py
@@ -1,38 +1,23 @@
 from post.models import Post
 from post.serializer import PostSerializer
 
-from rest_framework import generics
-from rest_framework import mixins
+from rest_framework import viewsets
 
-class PostList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-    #등록(generics.GenericAPIView 안에서 선언된 변수임)
+from rest_framework import renderers
+from rest_framework.decorators import action
+from django.http import HttpResponse
+
+'''
+class PostViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
+'''
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
     #필요로 하는 메서드
     #가변인자 사용(인자의 개수에 상관없이 받음)
-    def get(self, request, *args, **kwargs):
-        #list는 ListModelMixin에서 이미 정의된 메서드
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        #create는 CreateModelMixin에서 이미 정의된 메서드
-        return self.create(request, *args, **kwargs)
-
-class PostDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        #직접 Response()를 리턴한다.
+        return HttpResponse("성공")
